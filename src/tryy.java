e53f3b8143ad7cfaa12879350ac308a3eed9eb3c
nebahabesha <nebiyu28@gmail.com>
2021-07-01 23:20:50 +0300

    display from a user

--- src/tryy.java
+++ src/tryy.java
@@ -3,7 +3,7 @@ public class tryy {
     public static void main(String[] args) {
        Scanner in = new Scanner(System.in);
         System.out.println("please enter your name");
-       String name = in.nextLine();
+       String name = in.nextLine().trim();
         System.out.println("your name is " + name);
     }
 }
