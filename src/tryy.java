544bd556453ba09510960d1d812bb81faa305708
nebahabesha <nebiyu28@gmail.com>
2021-07-01 23:13:51 +0300

    display from a user

---
+++ src/tryy.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class tryy {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+        System.out.println("please enter your name");
+       String name = in.nextLine();
+        System.out.println("your name is " + name);
+    }
+}
